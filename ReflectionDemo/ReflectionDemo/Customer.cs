80bbf7bbcd6a83c5e3561d1d08a99117aa5f78fc
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-11 00:07:19 +0530

    ReflectionDemo

---
+++ ReflectionDemo/ReflectionDemo/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Customer
+    {
+        public int a, b;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer()//parameterless constructor
+        {
+            Id = 101;
+            Name = "Shreya";
+        }
+        public Customer(int id)//parameterised constructor
+        {
+            Id = id;
+        }
+        public void Print()//parameterless method
+        {
+
+        }
+        public void Test(int a)//parameterised method
+        {
+
+        }
+
+    }
+}
---
+++ ReflectionDemo/ReflectionDemo/ReflectionDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class ReflectionDemo
+    {
+        // reflection is process of discribing the metadata of type methord and field in a code
+        // metadeta refers to a binary information saved in a memory or a language runtime portable executable file
+        // Metadata inclued type defenation version infomation external assembly
+        //why Metadata? - understandable data, sorting or searching data easil
+
+        public class ReflectionClass
+        {
+
+        }
+    }
+}
